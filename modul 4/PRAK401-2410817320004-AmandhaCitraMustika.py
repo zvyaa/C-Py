0e97e66d61f1cede6cc9390b961eb2fe78d950ce
zvyaa <amandhacitra05@gmail.com>
2024-12-29 00:09:17 +0800

    Delete modul 4 directory

--- modul 4/PRAK401-2410817320004-AmandhaCitraMustika.py
+++
@@ -1,7 +0,0 @@
-a,b = input().  split()
-a = int(a)
-for i in range(1,51):
-    if i%a == 0:
-        print(b, end = " ")
-    else :
-        print(i,end = " ")
--- modul 4/PRAK402-2410817320004-AmandhaCitraMustika.py
+++
@@ -1,9 +0,0 @@
-a = int (input())
-for i in range(1, a+1):
-    if i%2!=0:
-        print(i, end=" ")
-print()
-
-for i in range(a,1,-1):
-    if i%2==0:
-        print(i, end=" ")
