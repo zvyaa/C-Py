86ad0f46e51822267a2ba8ef70c7ea68e9a74d75
zvyaa <amandhacitra05@gmail.com>
2024-12-29 00:08:58 +0800

    Delete modul 2 directory

--- modul 2/PRAK202-2410817320004-AmandhaCitraMustika.py
+++
@@ -1,8 +0,0 @@
-NP = float(input("Masukkan Nilai Pertama : "))
-NK = float(input("Masukkan Nilai Kedua : "))
-hasil = NP + NK
-
-if NP.is_integer():
-    print(f"Hasil dari penjumlahan nilai pertama \"{int(NP)}\" dan nillai kedua \"{NK: .1f}\" adalah \"{hasil: .2f}\"")
-else : 
-    print(f"Hasil dari penjumlahan nilai pertama \"{NP: .2f}\" dan nilai kedua \"{NK: .1f}\" adalah \"{hasil: .2f}\"")
--- modul 2/PRAK203-2410817320004-AmandhaCitraMustika.py
+++
@@ -1,9 +0,0 @@
-a = float(input())
-b = float(input())
-i = float(input())
-j = float(input())
-x = float(input())
-y = float(input())
-
-Hasil = (((a-b)*i)/j) - (x+y)
-print("%.3f"%(Hasil))
